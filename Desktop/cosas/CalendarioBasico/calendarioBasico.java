abafde1fa275e95e315bd33591caf2617f754959
Diego Antonio <diegoantonioalmontezarzuela.com>
2017-11-01 20:40:26 +0100

    Ahora tenemos un método que nos da una fecha predeterminada que es 01-01-01

--- Desktop/cosas/CalendarioBasico/calendarioBasico.java
+++ Desktop/cosas/CalendarioBasico/calendarioBasico.java
@@ -18,4 +18,15 @@ public class calendarioBasico
         mes = 0;
         anno = 0;
     }
+    
+    /**
+     * Esto es un método donde nos da una fecha predeterminada al invocarlo.
+     */
+    public String obtenerFecha()
+    {
+        dia = 01;
+        mes = 01;
+        anno = 01;
+        return dia + "-" + mes + "-" + anno;
+    }    
 }
