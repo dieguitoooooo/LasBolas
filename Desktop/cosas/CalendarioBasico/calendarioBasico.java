4e14741b653e8b89a82b03a8b501108d28c851b7
Diego Antonio <diegoantonioalmontezarzuela.com>
2017-11-01 20:38:00 +0100

    En este primer commit he configurado lo básico tal como, la clase y el constructor de esta

---
+++ Desktop/cosas/CalendarioBasico/calendarioBasico.java
@@ -0,0 +1,21 @@
+/**
+ * Esta es la clase de CalendarioBasico.
+ */
+public class calendarioBasico
+{
+    // Campos o atributos de la clase.
+    private int dia;
+    private int mes;
+    private int anno;
+        
+    /**
+     * Constructor de la clase calendario
+     */
+    public calendarioBasico()
+    {
+        // Aqui le damos un valor inicial a los campos.
+        dia = 0;
+        mes = 0;
+        anno = 0;
+    }
+}
