d4bd774cfd7116b56511ff8500b5d252aaee25d0
Diego Antonio <diegoantonioalmontezarzuela.com>
2018-02-22 20:12:44 +0100

    Modificado el metodo bouce, ahora podemos indicar por parametro cuantas bolas queremos.

---
+++ Desktop/0100/BallDemo.java
@@ -0,0 +1,56 @@
+import java.awt.Color;
+
+/**
+ * Class BallDemo - a short demonstration showing animation with the 
+ * Canvas class. 
+ *
+ * @author Michael Kölling and David J. Barnes
+ * @version 2011.07.31
+ */
+
+public class BallDemo   
+{
+    private Canvas myCanvas;
+
+    /**
+     * Create a BallDemo object. Creates a fresh canvas and makes it visible.
+     */
+    public BallDemo()
+    {
+        myCanvas = new Canvas("Ball Demo", 600, 500);
+    }
+
+    /**
+     * Simulate two bouncing balls
+     */
+    public void bounce(int nBolas)
+    {
+        int ground = 400;   // position of the ground line
+
+        myCanvas.setVisible(true);
+
+        // draw the ground
+        myCanvas.drawLine(50, ground, 550, ground);
+        
+        //Creamos numero de bolas indicadas por parametro
+        for(int i=0; i<nBolas; i++)
+        {
+            BouncingBall bola = new BouncingBall(30 + (20*i), 30, 15, Color.RED, ground, myCanvas);
+            bola.draw();
+        }
+
+        // crate and show the balls
+
+        // make them bounce
+        /*boolean finished =  false;
+        while(!finished) {
+            myCanvas.wait(50);           // small delay
+            ball.move();
+            ball2.move();
+            // stop once ball has travelled a certain distance on x axis
+            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
+                finished = true;
+            }
+        }*/
+    }
+}
